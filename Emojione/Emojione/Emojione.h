80ff4416d22e5ade57a75a5c36f6e8d7990d3b19
alexyip123 <alexyip123@gmail.com>
2018-06-22 10:17:39 -0700

    Initial commit

---
+++ Emojione/Emojione/Emojione.h
@@ -0,0 +1,19 @@
+//
+//  Emojione.h
+//  Emojione
+//
+//  Created by Lai Yip on 6/22/18.
+//  Copyright © 2018 Emojione. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Emojione.
+FOUNDATION_EXPORT double EmojioneVersionNumber;
+
+//! Project version string for Emojione.
+FOUNDATION_EXPORT const unsigned char EmojioneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Emojione/PublicHeader.h>
+
+
---
+++ EmojioneExamples/EmojioneExamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojioneExamples
+//
+//  Created by Lai Yip on 6/22/18.
+//  Copyright © 2018 Emojione. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
